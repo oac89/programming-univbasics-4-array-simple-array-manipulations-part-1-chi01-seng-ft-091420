82de99a00c49b13c7e4a9438f23d7df62fda219f
Oscar A Castro <oacastro2@gmail.com>
2020-08-25 21:11:07 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array, string)
+  array.push(string)
+end  
+
+def using_unshift(array, string)
+  array.unshift(string)
+end   
+
+def using_pop(array)
+  element = array.pop
+  
+end   
+
+def pop_with_args(array)
+  parameter.array.pop 
+  parameter.array.pop 
+  array.push()
+  
+end
+
+def using_shift(array)
+  parameter = array.shift 
+
+end
+
+def shift_with_args(array , argument)
+    parameter = array.pop
+    parameter = array.pop 
+    array.unshift(argument)
+    
+end   
